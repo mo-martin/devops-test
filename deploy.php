3832201a7ea54a47e54d3c8c060690fce0e0c395
Moses Martin <moses.martin@skybettingandgaming.com>
2016-09-20 14:17:19 +0100

    php test2

--- deploy.php
+++ deploy.php
@@ -1,6 +1,14 @@
 <?php
-  /*$message=shell_exec("/www/deploy.sh");*/
+  /*$message=shell_exec("/www/deploy.sh");
   exec("git pull origin master");
-  print_r($message);
+  print_r($message);*/
+
+define('PRIVATE_KEY', 'id_rsa');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'
+        && $_REQUEST['thing'] === PRIVATE_KEY)
+{
+    echo shell_exec("git pull");
+}
   echo "Code Deployed"
  ?>
